9c4cd64166416555cbee958d9e80c852d7402ab3
Daniel Alcalde <d.alcalde.puente@fz-juelich.de>
2022-09-21 17:02:16 +0200

    added examples

--- examples/test_script_instances.py
+++ examples/test_script_instances.py
@@ -5,9 +5,9 @@ import os
 import time
 
 time.sleep(2)
-
-save_name = "test_data.p"
+i = os.environ["ENCAP_INSTANCE"]
+save_name = f"test_data_{i}.p"
 print(save_name)
-print("This is the", os.environ["ENCAP_INSTANCE"], "encap instance")
+print("This is the", i, "encap instance")
 
 pickle.dump(["Some", "test", "data"], open(save_name, "wb"))
